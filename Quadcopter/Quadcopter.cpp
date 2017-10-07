59707a7a747506542e62e70af0d15f6f066a75b6
Montvydas <montvydas.klumbys@gmail.com>
2017-10-07 23:43:34 +0100

    Improved testMotors to use quadcopter library. Fixed quadcopter library to work

--- Quadcopter/Quadcopter.cpp
+++ Quadcopter/Quadcopter.cpp
@@ -9,20 +9,30 @@
 // constructor
 Quadcopter::Quadcopter(int m0, int m1, int m2, int m3)
 {
+  // Front Left
   this->motor[0] = m0;
+  // Front Right
   this->motor[1] = m1;
+  // Back Left
   this->motor[2] = m2;
+  // Back Right
   this->motor[3] = m3;
 
   // set all pins to be output
   for (int i = 0; i < 4; i++)
+  {
     pinMode(this->motor[i], OUTPUT);
+    this->speed[i] = 0;
+  }
 }
 
-void uadcopter::setConstantSpeed(int speed)
+void Quadcopter::setConstantSpeed(int speed)
 {
   for (int i = 0; i < 4; i++)
+  {
     analogWrite(this->motor[i], speed);
+    this->speed[i] = speed;
+  }
 }
 
 void Quadcopter::arm()
@@ -37,9 +47,11 @@ void Quadcopter::disarm()
 
 void Quadcopter::setSpeed(int* speed)
 {
-  this->speed = speed;
   for (int i = 0; i < 4; i++)
+  {
     analogWrite(this->motor[i], speed[i]);
+    this->speed[i] = speed[i];
+  }
 }
 
 void Quadcopter::setSingleMotor(int motor_index)
@@ -48,7 +60,11 @@ void Quadcopter::setSingleMotor(int motor_index)
   this->disarm();
 
   if (motor_index >= 0 && motor_index <= 3)
+  {
+    // this could be improved to use setSpeed instead of reinventing a function
     analogWrite(this->motor[motor_index], ARM_SPEED);
+    this->speed[motor_index] = ARM_SPEED;
+  }
 }
 
 int* Quadcopter::getSpeed()
@@ -61,12 +77,15 @@ int* Quadcopter::getStabilisedSpeed(int* speed, float rollDiff, float pitchDiff)
   //actual Speed is calculated as follows +- half rollDiff +- half pitchDiff
   this->speed[0] = (int) speed[0] + (rollDiff) - (pitchDiff);
   this->speed[1] = (int) speed[1] + (rollDiff) + (pitchDiff);
-  this->speed[2] = (int) speed[2] - (rollDiff) + (pitchDiff);
-  this->speed[3] = (int) speed[3] - (rollDiff) - (pitchDiff);
+  this->speed[2] = (int) speed[2] - (rollDiff) - (pitchDiff);
+  this->speed[3] = (int) speed[3] - (rollDiff) + (pitchDiff);
 
-  for (int i = 0; i < 4; i ++) {
+
+  for (int i = 0; i < 4; i++) {
     if (this->speed[i] < 0 )
       this->speed[i] = 0;
+    if (this->speed[i] > 255)
+      this->speed[i] = 255;
   }
   return this->speed;
 }
